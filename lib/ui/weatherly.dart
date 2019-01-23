531610716435c741d62c41884b2ba402c4ca5c2e
Timothy Freebern II <tfreebern2@gmail.com>
2019-01-22 21:04:34 -0500

    Created TODOs 1-3

---
+++ lib/ui/weatherly.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Weatherly extends StatefulWidget {
+  @override
+  _WeatherlyState createState() => _WeatherlyState();
+}
+
+class _WeatherlyState extends State<Weatherly> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO 1: Build AppBar
+    return Container();
+    // TODO 2: Create Image Widget for Weather
+
+
+
+
+    // TODO 3: Create TextWidgets for Temperature, Humidity, Max & Min Temps
+  }
+}

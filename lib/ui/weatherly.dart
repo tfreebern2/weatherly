fa2b4969b9d8bfa16317ed45ae4458e2a7beea26
Timothy Freebern II <tfreebern2@gmail.com>
2019-01-23 18:46:48 -0500

    Completed TODOs from Part2

--- lib/ui/weatherly.dart
+++ lib/ui/weatherly.dart
@@ -6,16 +6,47 @@ class Weatherly extends StatefulWidget {
 }
 
 class _WeatherlyState extends State<Weatherly> {
-
   @override
   Widget build(BuildContext context) {
-    // TODO 1: Build AppBar
-    return Container();
-    // TODO 3: Create Image Widget for Weather
-
-
-
-
-    // TODO 4: Create TextWidgets for Temperature, Humidity, Max & Min Temps
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weatherly'),
+        backgroundColor: Colors.redAccent,
+        actions: <Widget>[
+          new IconButton(
+              icon: new Icon(Icons.menu),
+              onPressed: () => debugPrint('Button Hit'))
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Image.asset(
+              'images/umbrella.png',
+              width: 490.0,
+              height: 1200.0,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Orlando',
+              style: TextStyle(color: Colors.white, fontSize: 32.0),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              'Current Temp: 85.0\n'
+                  'Humidity: 81\n'
+                  'Min Temp: 62.0\n'
+                  'Max Temp: 90.0',
+              style: TextStyle(color: Colors.white, fontSize: 22.0),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }

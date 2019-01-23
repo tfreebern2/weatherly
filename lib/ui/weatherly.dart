ec0c3286218e831947d3e66f7fe1487bee865ba8
Timothy Freebern II <tfreebern2@gmail.com>
2019-01-23 18:50:57 -0500

    Added TODO 5. Edited TODO 1

--- lib/ui/weatherly.dart
+++ lib/ui/weatherly.dart
@@ -9,13 +9,15 @@ class _WeatherlyState extends State<Weatherly> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO 1: Build AppBar
+    // TODO 1: Build AppBar with Title & BackgroundColor
     return Container();
-    // TODO 3: Create Image Widget for Weather
+    // TODO 3: Create Image Widget for Background
 
 
 
 
     // TODO 4: Create TextWidgets for Temperature, Humidity, Max & Min Temps
+
+    // TODO 5: Create TextField Widget to Enter New City
   }
 }

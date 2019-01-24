e10d5f38d90c8abf5bd62a672a710774c9a86c62
Timothy Freebern II <tfreebern2@gmail.com>
2019-01-24 18:56:08 -0500

    Added Utils.dart with vars

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,3 @@ import 'package:flutter/material.dart';
 import 'package:weatherly/ui/weatherly.dart';
 
 void main() => runApp(MaterialApp(theme: ThemeData.light(), home: Weatherly()));
-
-// TODO 1: Create util package under library directory
-// TODO 2: In utils.dart add AppID and defaultCity final variables
-// TODO 3: Add utils.dart to .gitignore

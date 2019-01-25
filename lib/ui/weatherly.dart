df084d80dc3fd583c7afba7a6f98508ae14a4e49
Timothy Freebern II <tfreebern2@gmail.com>
2019-01-25 12:34:02 -0500

    getWeather debugPrint works

--- lib/ui/weatherly.dart
+++ lib/ui/weatherly.dart
@@ -35,17 +35,18 @@ class _WeatherlyState extends State<Weatherly> {
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: updateTempWidget(
+                child:
+//                updateTempWidget(_changeCityController.text
+                  Text(
                   'Current Temp: 85.0\n'
                       'Humidity: 81\n'
                       'Min Temp: 62.0\n'
                       'Max Temp: 90.0',
-                  style: TextStyle(color: Colors.black, fontSize: 22.0),
-                ),
+                    ),
               ),
               Padding(
                 padding:
-                const EdgeInsets.only(left: 30.0, right: 30.0, top: 10.0),
+                    const EdgeInsets.only(left: 30.0, right: 30.0, top: 10.0),
                 child: TextField(
                   decoration: InputDecoration(
                       hintText: 'Enter City',
@@ -55,9 +56,9 @@ class _WeatherlyState extends State<Weatherly> {
                   keyboardType: TextInputType.text,
                 ),
               ),
-              RaisedButton(onPressed: () {
-                debugPrint(_changeCityController.text);
-              },
+              RaisedButton(
+                onPressed: () =>
+                    (getWeather(util.appId, _changeCityController.text)),
                 textColor: Colors.white,
                 color: Colors.redAccent,
                 child: Text('Get Weather'),
@@ -67,47 +68,46 @@ class _WeatherlyState extends State<Weatherly> {
         ],
       ),
     );
-    // TODO 5: Create updateWeatherWidget() that returns a FutureBuilder
   }
+
   Future<Map> getWeather(String appId, String city) async {
     String apiUrl =
         'http://api.openweathermap.org/data/2.5/weather?q=$city&appid='
         '${util.appId}&units=imperial';
 
     http.Response response = await http.get(apiUrl);
-
+    print(json.decode(response.body));
     return json.decode(response.body);
   }
 
   Widget updateTempWidget(String city) {
-    return new FutureBuilder(
+    return FutureBuilder(
         future: getWeather(util.appId, city == null ? util.defaultCity : city),
         builder: (BuildContext context, AsyncSnapshot<Map> snapshot) {
           //where we get all of the json data, we setup widgets etc.
           if (snapshot.hasData) {
             Map content = snapshot.data;
-            return new Container(
+            return Container(
               margin: const EdgeInsets.fromLTRB(30.0, 250.0, 0.0, 0.0),
-              child: new Column(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  new ListTile(
-                    title: new Text(
-                      content['main']['temp'].toString() +" F",
-                      style: new TextStyle(
+                  ListTile(
+                    title: Text(
+                      content['main']['temp'].toString() + " F",
+                      style: TextStyle(
                           fontStyle: FontStyle.normal,
                           fontSize: 49.9,
                           color: Colors.white,
                           fontWeight: FontWeight.w500),
                     ),
-                    subtitle: new ListTile(
-                      title: new Text(
-                        "Humidity: ${content['main']['humidity'].toString()}\n"
+                    subtitle: ListTile(
+                      title: Text(
+                        "Current: ${content['main']['temp'].toString()} F\n"
+                            "Humidity: ${content['main']['humidity'].toString()}\n"
                             "Min: ${content['main']['temp_min'].toString()} F\n"
                             "Max: ${content['main']['temp_max'].toString()} F ",
-
-                        style: (),
-
+                        style: TextStyle(color: Colors.black),
                       ),
                     ),
                   )
@@ -115,12 +115,8 @@ class _WeatherlyState extends State<Weatherly> {
               ),
             );
           } else {
-            return new Container();
+            return Container();
           }
         });
   }
 }
-
-
-
-
